c5e8fb5f0716a3833ea9750317ef3af28418f74a
Viktory4121 <medvedka4121@gmail.com>
2023-06-18 22:55:01 +0300

    add volume-block

---
+++ scripts/script-click-slyder.js
@@ -0,0 +1,8 @@
+/* $('input[type="radio"]').click(function(){
+    $(".vol-button-img").css({"background": $(this).val()});
+}); */
+
+/* $('input[type="radio"]').click(function(){
+    $(".vol-button-img").css({"backgroundImage": "url(" + "../images/trucks/truck-2000.png" + ")"});
+}); */
+
