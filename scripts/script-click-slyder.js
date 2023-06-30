8ddca4731f184a2ec2d67b8d2b05b6ae99e300dc
Viktory4121 <medvedka4121@gmail.com>
2023-06-30 13:07:33 +0300

    add new sections and fix

--- scripts/script-click-slyder.js
+++
@@ -1,8 +0,0 @@
-/* $('input[type="radio"]').click(function(){
-    $(".vol-button-img").css({"background": $(this).val()});
-}); */
-
-/* $('input[type="radio"]').click(function(){
-    $(".vol-button-img").css({"backgroundImage": "url(" + "../images/trucks/truck-2000.png" + ")"});
-}); */
-
